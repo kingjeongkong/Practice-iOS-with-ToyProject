f5a2161d3b0a4677a58a3186a53a9ad75e42082c
kingjeongkong <wjdqls9203@naver.com>
2022-08-15 23:15:56 +0900

    로그인 앱 UI 구성

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -21,6 +21,11 @@ class EnterEmailViewController: UIViewController {
         nextButton.layer.cornerRadius = 30
         
         nextButton.isEnabled = false
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        emailTextField.becomeFirstResponder() //커서가 바로 위치할 수 있게 해주는 함수
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,4 +35,22 @@ class EnterEmailViewController: UIViewController {
         navigationController?.navigationBar.isHidden = false
     }
     
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
+extension EnterEmailViewController: UITextFieldDelegate {
+    //작성 완료 후 키보드 내려감
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    //작성 완료하면 NextButton 활성화
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let isEmailEmpty = emailTextField.text == ""
+        let isPasswordEmpty = passwordTextField.text == ""
+        nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
+    }
 }
---
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 왕정빈 on 2022/08/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+}
