fb9632983e08913972d187036ac25b39ac3e1031
kingjeongkong <wjdqls9203@naver.com>
2022-08-25 14:26:50 +0900

    신용카드 추천 앱 UI 구성 완료

---
+++ CreditCardList/CreditCardList/CardDetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CardDetailViewController.swift
+//  CreditCardList
+//
+//  Created by 왕정빈 on 2022/08/24.
+//
+
+import UIKit
+import Lottie
+
+class CardDetailViewController: UIViewController {
+    
+    var promotionDetail: PromotionDetail?
+    
+    @IBOutlet weak var lottieView: AnimationView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var benefitConditionLabel: UILabel!
+    @IBOutlet weak var benefitDetailLabel: UILabel!
+    @IBOutlet weak var benefitDateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let animationView = AnimationView(name: "money")
+        lottieView.contentMode = .scaleAspectFit
+        lottieView.addSubview(animationView)
+        animationView.frame = lottieView.bounds
+        animationView.loopMode = .loop
+        animationView.play()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let detail = promotionDetail else { return }
+        
+        titleLabel.text = """
+            \(detail.companyName)카드 쓰면
+            \(detail.amount)만원 드려요
+            """
+        
+        periodLabel.text = detail.period
+        conditionLabel.text = detail.condition
+        benefitConditionLabel.text = detail.benefitCondition
+        benefitDetailLabel.text = detail.benefitDetail
+        benefitDetailLabel.text = detail.benefitDate
+    }
+    
+}
--- CreditCardList/CreditCardList/CardListViewController.swift
+++ CreditCardList/CreditCardList/CardListViewController.swift
@@ -6,7 +6,48 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CardListViewController: UITableViewController {
+    
+    var creditCardList: [CreditCard] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UITableView Cell Register
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() }
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //상세화면 전달
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDetailViewController") as? CardDetailViewController else { return }
+        
+        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
+        self.show(detailViewController, sender: nil)
+    }
     
 }
